970eca66b704db6a244b62f65230fd9fce9c5b99
bextl <140372518+bextl@users.noreply.github.com>
2023-11-09 18:40:14 +0600

    version 1
    still working on this project

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="app">
+        <post-form/>
+        <post-list :posts = "posts"/>
+        
+            
+        
+       
+
+    </div>
+</template>
+
+<script>
+import PostForm from "./components/PostForm"
+import PostList from "./components/PostList"
+
+
+export default{
+    components: {
+        PostList, PostForm
+    }, 
+
+    data(){
+        return{
+        posts: [
+            {id: 1, title: 'Javascript', body: 'Description of the post'},
+            {id: 2, title: 'Javascript 2', body: 'Description of the post 2 '},
+            {id: 3, title: 'Javascript 3', body: 'Description of the post 3'},
+            {id: 4, title: 'Javascript 4', body: 'Description of the post 4'},
+        ],
+        title: '',
+        body: '',
+        
+    }
+}, 
+methods: {
+    createPost(){
+        const newPost = {
+            id: Date.now(),
+            title: this.title,
+            body: this.body,
+        }
+        this.posts.push(newPost);
+        this.title = '';
+        this.body = '';
+
+    },
+    // inputTitle(event) {
+       
+    //     this.title = event.target.value;
+
+    // }
+
+},
+
+}
+
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/PostForm.vue
@@ -0,0 +1,73 @@
+
+<template>
+<form @submit.prevent>
+    <h3>Creating a post</h3>
+    <input 
+    v-bind:value="post.title" 
+    @input="post.title = $event.target.value"
+    class="input" 
+    type="text" 
+    placeholder="name">
+
+    <input 
+    v-bind:value="post.body"
+    @input="post.body = $event.target.value"
+    class="input" 
+    type="text"
+     placeholder="description">
+
+    <button class="btn">Ad d Post</button>
+</form>
+
+
+</template>
+
+
+<script>
+export default{
+    data(){
+        return {
+            post: {
+                title: '',
+                body: '',
+            }
+        }
+    }
+}
+
+
+</script>
+
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+}
+
+form{
+    display: flex;
+    flex-direction: column;
+}
+.input{
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px; 
+    margin-top: 15px;
+}
+.btn{
+    align-self: flex-end;
+    margin-top: 15px;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+
+.app{
+    padding: 20px;
+}
+
+
+</style>
